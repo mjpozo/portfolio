3c3aeb3e933cb7bbbc79d20ccc9e299daf215e45
mjpozo <mariaj.pozom@gmail.com>
2019-03-07 04:52:42 -0300

    about y tool listos en version desktop y mobile + imagenes y base para portafolio

--- dist/js/script.js
+++ dist/js/script.js
@@ -14896,4 +14896,33 @@ function shuffleArray(array)
     }
     return array;
 }
-//alert("testeando js...");
+
+$(document).ready(function(){
+
+	//nav cambia de color cuando se baja la página
+	$(document).scroll(function(){
+		if ($(this).scrollTop() > 90){
+			$(".header .cont-opacity nav").addClass("nav-scroll");
+		} else if ($(this).scrollTop() <= 90){
+			$(".header .cont-opacity nav").removeClass("nav-scroll");
+		}
+	});
+
+	//smoth scrolling
+	$('a[href*="#"]:not([href="#"])').click(function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	    	var target = $(this.hash);
+	    	target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	    	if (target.length) {
+		        $('html, body').animate({
+		        	scrollTop: target.offset().top
+		        }, 1000);
+		        return false;
+	    	}
+	    }
+	});
+
+	// ABOUT ME
+	$('.carousel').carousel();
+
+})
